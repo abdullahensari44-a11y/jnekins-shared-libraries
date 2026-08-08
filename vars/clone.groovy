3bc0cd11855e1be014923f0ccf35fd6f83d68bf6
abdullahensari44-a11y <abdullahensari44@gmail.com>
2026-08-08 16:13:09 +0500

    Fix indentation in clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def call(String url,String branch){
   
-                git url:"${url}" , branch: "${branch}",
+                git url: "${url}" , branch: "${branch}",
                    
   
             }
